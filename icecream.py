a5be6d3f56c933d468ef11f6563344a01e78bb4f
Justin Schrantz <jschrantz1@users.noreply.github.com>
2022-04-18 22:13:02 +0000

    icecream.py

---
+++ icecream.py
@@ -0,0 +1,14 @@
+icecream= ["indentation", "spaces"]
+
+tlgstudents= ["Akino", "Bai", "Carlos", "Dalton", "Dan", "Edith", "Ethan", "Isaiah", "J", "Jessica", "John", "Justin", "Khalil", "Nikk", "Ramesh", "Scotty", "Sergio", "Shawn"]
+
+icecream.append(4)
+
+#choice= input("Pick a student number!")
+#choice= int(input("Pick a student number!"))
+
+choice= int(input("Pick a student number!"))
+
+student_name= tlgstudents[choice]
+
+print(f"{student_name} is awesome!")
