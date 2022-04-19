5941d3e5e5e4af1a8d33d537bd62dc2391d8203c
Justin Schrantz <jschrantz1@users.noreply.github.com>
2022-04-19 18:02:42 +0000

    pokedex.py

--- pokedex.py
+++ pokedex.py
@@ -2,6 +2,24 @@ pokedex= {"Bulbasaur":"Grass/Poison",
          "Squirtle":"Water",
          "Charmander":"Fire"}
 
-choice= input("Name a Generation 1 starter Pokemon:\n>")
-#print( switch.get("lynx", "THE KEY IS IN ANOTHER CASTLE!") )
-print(pokedex.get(choice, "Sorry we dont have any record of that Pokemon!"))
+pokedex["Pikachu"] = "Electric"
+
+a = pokedex
+a = ",".join(a)
+print(a)
+
+
+b = "Sorry we dont have any record of that Pokemon!"
+
+choice= input("Choose a starter Pokemon from the list:\n>")
+
+print(pokedex.get(choice, b))
+
+        # if the pokemon you chose was not in the available pokemon
+if choice not in pokedex:
+       print("Please make a different selection")
+else:
+    print("Great choice!", (pokedex.get(choice)), "is a favorite type of mine! Choose another Pokemon!")
+
+#print(pokedex.get(choice, "Sorry we dont have any record of that Pokemon!"))
+        
