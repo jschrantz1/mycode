f301e4c74028017d2ef276fdbf776a81b146ba29
Justin Schrantz <jschrantz1@users.noreply.github.com>
2022-04-19 18:35:01 +0000

    pokedex.py

--- pokedex.py
+++ pokedex.py
@@ -21,5 +21,12 @@ if choice not in pokedex:
 else:
     print("Great choice!", (pokedex.get(choice)), "is a favorite type of mine! Choose another Pokemon!")
 
-#print(pokedex.get(choice, "Sorry we dont have any record of that Pokemon!"))
+choice= input("Choose a starter Pokemon from the list:\n>")
+
+print(pokedex.get(choice, b))
+
+if choice not in pokedex:
+    print("Please make a different selection")
+else:
+    print("Great choice!", (pokedex.get(choice)), "is a favorite type of mine! Choose another Pokemon!")
         
