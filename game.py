b8981635e1e873647356e78190e975e3ca2fcc09
Justin Schrantz <jschrantz1@users.noreply.github.com>
2022-04-24 20:14:11 +0000

    game

--- game.py
+++ game.py
@@ -62,7 +62,10 @@ rooms = {
               },
             'Basement' : {
                   'south' : 'Hall',
-                  'item' : 'whip and chains.'
+                  'item' : 'whip and chains.',
+                  'east' : 'B1',
+                  'west' : 'B1',
+                  'north' : 'B1'
                    },
             'Upstairs Hall' : {
                   'south' : 'Playroom',
@@ -98,6 +101,30 @@ rooms = {
             'Attic' : {
                     'west' : 'Upstairs Hall',
                     'item' : 'dead thai lady boy'
+                    },
+            'B1' : {
+                    'west' : 'Basement',
+                    'east' : 'B3',
+                    'north' : 'B2',
+                    'south' : 'B2'
+                    },
+            'B2' : {
+                    'west' : 'B4',
+                    'east' : 'B4',
+                    'north' : 'B1',
+                    'south' : 'B1'
+                    },
+            'B3' : {
+                    'west' : 'B1',
+                    'east' : 'B3',
+                    'north' : 'B3',
+                    'south' : 'B4'
+                    },
+            'B4' : {
+                    'west' : 'B2',
+                    'east' : 'B2',
+                    'north' : 'B3',
+                    'south' : 'B3'
             }
          }
 
