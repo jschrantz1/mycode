a70e137ec9556d0d74708332748e96b3307f0cbe
Justin Schrantz <jschrantz1@users.noreply.github.com>
2022-04-24 20:16:02 +0000

    game1

---
+++ maze.py
@@ -0,0 +1,31 @@
+ 'Basement' : {
+                  'south' : 'Hall',
+                  'item' : 'whip and chains.'
+                  'item' : 'whip and chains.',
+                  'east' : 'B1',
+                  'west' : 'B1',
+                  'north' : 'B1'
+                   },
+            'B1' : {
+                    'west' : 'Basement',
+                    'east' : 'B3',
+                    'north' : 'B2',
+                    'south' : 'B2'
+                    },
+            'B2' : {
+                    'west' : 'B4',
+                    'east' : 'B4',
+                    'north' : 'B1',
+                    'south' : 'B1'
+                    },
+            'B3' : {
+                    'west' : 'B1',
+                    'east' : 'B3',
+                    'north' : 'B3',
+                    'south' : 'B4'
+                    },
+            'B4' : {
+                    'west' : 'B2',
+                    'east' : 'B2',
+                    'north' : 'B3',
+                    'south' : 'B3'
